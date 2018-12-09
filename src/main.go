6759ca4ee1aced3f7fd8392ef31252c0dda70c97
qtn0224 <qtn0224@ATL42QTN032621.local>
2018-12-09 15:13:08 -0500

    changed code

--- src/main.go
+++ src/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
+// func main() {
+// 	http.HandleFunc("/", handler)
+// 	log.Fatal(http.ListenAndServe(":3001", nil))
+// }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":3001", nil))
+	for true {
+		fmt.Println("Infinite Loop 2")
+		time.Sleep(time.Second)
+	}
 }
